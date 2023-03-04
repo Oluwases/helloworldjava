77f075c17bd323b945003f6371c3c3fd2d1b17e8
Oluwases <108632627+Oluwases@users.noreply.github.com>
2023-03-03 19:04:03 -0500

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World - March 3rd 2023 - 07:00 PM poll scm/cron job");
+    System.out.println("Hello World - March 3rd 2023 - 07:00 PM job triggered by webhooks");
     }
 }
